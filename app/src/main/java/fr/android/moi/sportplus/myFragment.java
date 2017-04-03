2f43771278aedd91f43363974c94758a382b5f74
tterence <terence.tadjifoue@gmail.com>
2017-04-03 22:13:51 +0200

    Commit1

---
+++ app/src/main/java/fr/android/moi/sportplus/myFragment.java
@@ -0,0 +1,109 @@
+package fr.android.moi.sportplus;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Térence on 03/04/2017.
+ */
+
+public class myFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_FRAG_TYPE = "map";
+    private static final String ARG_PARAM2 = "param2";
+    // The request code must be 0 or greater.
+    private static final int PLUS_ONE_REQUEST_CODE = 0;
+    // The URL to +1. Must be a valid URL.
+    private final String PLUS_ONE_URL = "http://developer.android.com";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    //private Button b1;
+    private OnFragmentInteractionListener mListener;
+    public myFragment() {
+        // Required empty public constructor
+    }
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment firstFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static myFragment newInstance(String param1, String param2) {
+        myFragment fragment = new myFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_FRAG_TYPE, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_FRAG_TYPE);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view;
+        if (mParam1=="map") {
+            view = inflater.inflate(R.layout.fragment_maps, container,
+                    false);
+        }else {
+            view = inflater.inflate(R.layout.content_main, container,
+                    false);
+        }
+        return view;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Refresh the state of the +1 button each time the activity receives focus.
+        //mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Object ref);
+    }
+}
